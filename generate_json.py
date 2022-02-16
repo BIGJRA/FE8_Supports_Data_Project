54d0b4236608c241021b54199b83d9774aa4a3a2
bigjra441 <jake.spammailz@gmail.com>
2022-02-16 15:11:06 -0500

    added new files

---
+++ generate_json.py
@@ -0,0 +1,104 @@
+import requests
+import lxml.html as lh
+import json
+#import random
+
+
+def get_table_rows(url, strip_word=None):
+    """ Takes as input a string containing url.
+    Returns a dictionary in the form of a tuple:
+    Index 0 contains the table column name,
+    Index 1 is a list of the text in the entries going down."""
+
+    # Page handles the contents of url
+    page = requests.get(url)
+
+    # Store the contents of page
+    doc = lh.fromstring(page.content)
+
+    # Parse data stored in <tr> tags
+    tr_elements = doc.xpath('//tr')
+
+    # Create empty list
+    col = []
+    i = 0
+    # For each row, store each first element (header) and an empty list
+    for t in tr_elements[0]:
+        i += 1
+        name = t.text_content()
+        # print ('%d:"%s"' % (i, name))
+        col.append((name, []))
+
+    # Since out first row is the header, data is stored on the second row onwards
+    for j in range(1, len(tr_elements)):
+        # T is our jth row
+        row_j = tr_elements[j]
+
+        # column
+        i = 0
+
+        # Iterate through each element of the row
+        for t in row_j.iterchildren():
+            data = t.text_content()
+
+            # Here it skips the row if the first element is strip_word. Helps solve a problem
+            # in the serenes forest link which has internal table headers.
+            if strip_word is not None and data == strip_word:
+                break
+
+            # Append the data to the empty list of the ith column
+            col[i][1].append(data)
+            # Increment i for the next column
+            i += 1
+
+    return {title: column for (title, column) in col}
+
+
+def convert_to_character_dict(dictionary):
+    """ Takes as input a column: values dictionary format that is obtained from serenes forest
+    and returns a dictionary with key: character and values: list of support partners,
+    represented as tuples (name, base, rate)."""
+
+    result = {}
+    for pos, character_name in enumerate(dictionary["Character"]):
+        character_name = character_name.replace(' ', '')
+        character_name = character_name.replace('’', "'")
+        result[character_name] = []
+        for option_num in range(1, len(dictionary)):
+
+            # ignores if the text is simply "-"
+            content = dictionary[f"Option {option_num}"][pos]
+            if content == "–":
+                break
+
+            # extracts the three parts from data and adds 2 fields
+            data = content.split()
+            # rand = random.random()
+            data_dict = {
+                "partner": data[0].replace('’', "'"),
+                "base": int(data[1]),
+                "rate": int(data[-1][1:]),
+                "finished": "N/A",
+                "in_progress": False
+            }
+
+            result[character_name].append(data_dict)
+
+    return result
+
+
+def write_to_json(filename, dictionary):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(dictionary))
+
+
+def main():
+    serenes = "https://serenesforest.net/the-sacred-stones/characters/supports/"
+    d = get_table_rows(serenes, strip_word="Character")
+    d = convert_to_character_dict(d)
+    write_to_json('support_data.json', d)
+    print("Generated support_data.json")
+
+
+if __name__ == "__main__":
+    main()
---
+++ process.py
@@ -0,0 +1,159 @@
+import json
+import random
+
+
+class SupportTracker:
+
+    def __init__(self, json_filename):
+        self.filename = json_filename
+        with open(self.filename) as f:
+            self.content = json.load(f)
+        self.support_conversations_dict = {"A": 3, "B": 2, "C": 1, "N/A": 0}
+
+    def save(self):
+        """ Saves self. content to json_filename."""
+        # print (self.content)
+        with open(self.filename, 'w') as f:
+            f.write(json.dumps(self.content))
+
+    def get_total_progress(self):
+        """ Returns as an integer percent (rounded down) the number of completed support conversations.
+        """
+        total_supports = 0
+        curr_supports = 0
+        for character in self.content:
+            for partner_data in self.content[character]:
+                total_supports += 3
+                curr_supports += self.support_conversations_dict[partner_data["finished"]]
+                # print (total_supports, curr_supports)
+        return int(curr_supports / total_supports * 100)
+
+    def get_current_pairs(self):
+        """ Returns as list of tuples the currently active pairs in the form
+        char1, char2, current support level
+        """
+        result = set([])
+        for character in self.content:
+            for partner_data in self.content[character]:
+                if partner_data["in_progress"]:
+                    char1, char2 = sorted([character, partner_data["partner"]])
+                    result.add((char1, char2, partner_data["finished"]))
+        return sorted(list(result))
+
+    def get_finished_pairs(self):
+        """ Returns as list of tuples the currently active pairs in the form
+        char1, char2
+        """
+        result = set([])
+        for character in self.content:
+            for partner_data in self.content[character]:
+                if partner_data["finished"] == "A":
+                    char1, char2 = sorted([character, partner_data["partner"]])
+                    result.add((char1, char2))
+        return sorted(list(result))
+
+    def get_unpaired_characters(self):
+        """ Returns list of characters which are not currently paired. """
+        result = []
+        for char in self.content:
+            skip = False
+            for pair in self.get_current_pairs():
+                if char == pair[0] or char == pair[1]:
+                    skip = True
+                    break
+            if not skip:
+                result.append(char)
+        return result
+
+    def get_new_pair(self):
+        """ Returns random tuple of characters with potential pairing from the unpaired characters.
+        Only picks from the characters which have not yet reached A support.
+        """
+        chars = self.get_unpaired_characters()
+        n = len(chars)
+        check_idx = random.randint(0, n - 1)
+        check_count = 0
+        while check_count < n:
+            curr = chars[check_idx]
+            m = len(self.content[curr])
+            partner_idx = random.randint(0, m - 1)
+            partner_count = 0
+            while partner_count < m:
+                partner_data = self.content[curr][partner_idx]
+                if partner_data["partner"] not in chars or partner_data["finished"] == "A":
+                    partner_idx = (partner_idx + 1) % m
+                    partner_count += 1
+                else:
+                    return curr, partner_data["partner"]
+            check_idx = (check_idx + 1) % n
+            check_count += 1
+        return None
+
+    def update_pair(self, char1, char2, level):
+        """ Updates the entry for char1, char2 to be the given support level.
+        """
+        i = 0
+        while self.content[char1][i]["partner"] != char2:
+            i += 1
+        self.content[char1][i]["finished"] = level
+        i = 0
+        while self.content[char2][i]["partner"] != char1:
+            i += 1
+        self.content[char2][i]["finished"] = level
+        self.save()
+
+    def set_current(self, char1, char2, is_current=True):
+        """ Sets char1, char2 to have in_progress value is_current.
+        """
+        i = 0
+        while self.content[char1][i]["partner"] != char2:
+            i += 1
+        self.content[char1][i]["in_progress"] = is_current
+        i = 0
+        while self.content[char2][i]["partner"] != char1:
+            i += 1
+        self.content[char2][i]["in_progress"] = is_current
+        self.save()
+
+    def start_new_run(self):
+        """ Resets all pairs to be not current. """
+        for char in self.content:
+            for partner_data in self.content[char]:
+                partner_data["in_progress"] = False
+        self.save()
+
+    def reset_all(self):
+        """ Resets all pairs to default. """
+        for char in self.content:
+            for partner_data in self.content[char]:
+                partner_data["finished"] = "N/A"
+                partner_data["in_progress"] = False
+        self.save()
+
+
+def demo_update(tracker):
+    tracker.reset_all()
+    tracker.update_pair("Eirika", "Ephraim", "A")
+    tracker.update_pair("Gilliam", "Franz", "A")
+    tracker.update_pair("Kyle", "Colm", "A")
+    tracker.update_pair("Joshua", "Natasha", "A")
+    tracker.update_pair("Tethys", "Gerik", "A")
+    tracker.update_pair("Artur", "Cormag", "A")
+    tracker.update_pair("Innes", "Tana", "A")
+    tracker.update_pair("Knoll", "Duessel", "A")
+    tracker.update_pair("Dozla", "L'Arachel", "A")
+    tracker.set_current("Eirika", "Ephraim")
+    tracker.set_current("Gilliam", "Franz")
+    tracker.set_current("Kyle", "Colm")
+    tracker.set_current("Joshua", "Natasha")
+    tracker.set_current("Tethys", "Gerik")
+    tracker.set_current("Artur", "Cormag")
+    tracker.set_current("Innes", "Tana")
+    tracker.set_current("Knoll", "Duessel")
+    tracker.set_current("Dozla", "L'Arachel")
+
+
+if __name__ == "__main__":
+    s = SupportTracker("support_data.json")
+    demo_update(s)
+    pass
