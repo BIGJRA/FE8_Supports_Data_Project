fc029ca13776d20339aa18ba4da69b31cf8b5a61
bigjra441 <jake.spammailz@gmail.com>
2022-02-17 12:13:54 -0500

    minor

--- get_images.py
+++ get_images.py
@@ -8,7 +8,7 @@ import os
 
 try:
     os.mkdir("img")
-except:
+except FileExistsError:
     pass
 
 site = 'https://serenesforest.net/the-sacred-stones/characters/introduction/'
@@ -20,16 +20,16 @@ img_tags = soup.find_all('img')
 
 urls = ["https://serenesforest.net" + img['src'] for img in img_tags]
 
-print (f"{len(urls)} urls found. Processing...")
-#print (urls)
+print(f"{len(urls)} urls found. Processing...")
+# print (urls)
 
 skip = 0
 for url in urls:
     filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
     if not filename:
-         print("Regex didn't match with the url: {}".format(url))
-         skip += 1
-         continue
+        print("Regex didn't match with the url: {}".format(url))
+        skip += 1
+        continue
     path = os.path.join("img", filename.group(1))
     with open(path, 'wb') as f:
         '''if 'http' not in url:
@@ -39,6 +39,6 @@ for url in urls:
             url = '{}{}'.format(site, url)'''
         response = requests.get(url)
         f.write(response.content)
-        print (f"Processed {(filename.group(0))}.")
+        print(f"Processed {(filename.group(0))}.")
 
-print (f"Of {len(urls)} urls found, {len(urls) - skip} processed and {skip} skipped.")
+print(f"Of {len(urls)} urls found, {len(urls) - skip} processed and {skip} skipped.")
